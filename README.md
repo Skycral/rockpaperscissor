b37b825af651683a4a38ea7a4ade6310dcf1b503
Skyborn <c.skyborn@hotmail.com>
2024-11-13 13:57:23 +0100

    Configure app to buttons instead of prompt
    The changes have been made to index, script and README. Text (pseudo)
    have been put into README. Index have been updated with buttons and div.
    Script now have three buttons, one for each choice. After pressed, the
    current score is displayed below the buttons.

--- README.md
+++ README.md
@@ -1,1 +1,48 @@
 # rockpaperscissor
+
+// Step Two 
+/*
+Create function getComputerChoice - will randomly choose one of three options
+Create three variables holding rock, scissor, paper
+Create algo that have a 33.3% chance for each variable to be returned
+- 0-0.33 Rock
+- 0.34 - 066 Paper
+- 0.67 - 0.99 Scissor
+Use if else to define the return
+
+*/
+
+/*Step 3
+Write fn getHumanChoice where user can choose btw rock, paper, scissor
+add a prompt where user writes choice - if no valid input, ask again
+add variables to hold the choice
+(add a multi choice to avoid any wrong doing?)
+*/
+
+//getHumanChoice();
+
+/*
+Step 4
+create two variables to hold pc and human scores
+they should both start at 0
+*/
+
+/*
+Step 5
+create a function playRound to play a round of the game
+define two params to take in the choices of pc and user 
+make the input choice case insensitive - use lowercase method
+use logic to understand who wins in each round - rock beats scissor
+scissor beats paper
+paper beats rock
+if logic where each one gives a point to the score 
+and writes out the winner in the log
+*/
+
+/*
+Step 6
+create a fn playGame to play 5 rounds
+playround should be inside this function and be repeated 5 times, with loop?
+need to get new choices from the computer each time
+return random value and then chose the best fit in this fn?
+*/
--- script.js
+++ script.js
@@ -1,138 +1,115 @@
 console.log("Hello World");
 
-// Step Two 
-/*
-Create function getComputerChoice - will randomly choose one of three options
-Create three variables holding rock, scissor, paper
-Create algo that have a 33.3% chance for each variable to be returned
-- 0-0.33 Rock
-- 0.34 - 066 Paper
-- 0.67 - 0.99 Scissor
-Use if else to define the return
-
-*/
-
-/*Step 3
-Write fn getHumanChoice where user can choose btw rock, paper, scissor
-add a prompt where user writes choice - if no valid input, ask again
-add variables to hold the choice
-(add a multi choice to avoid any wrong doing?)
-*/
-
-//getHumanChoice();
-
-/*
-Step 4
-create two variables to hold pc and human scores
-they should both start at 0
-*/
-
-/*
-Step 5
-create a function playRound to play a round of the game
-define two params to take in the choices of pc and user 
-make the input choice case insensitive - use lowercase method
-use logic to understand who wins in each round - rock beats scissor
-scissor beats paper
-paper beats rock
-if logic where each one gives a point to the score 
-and writes out the winner in the log
-*/
-
-/*
-Step 6
-create a fn playGame to play 5 rounds
-playround should be inside this function and be repeated 5 times, with loop?
-need to get new choices from the computer each time
-return random value and then chose the best fit in this fn?
-*/
-
 
 let humanScore = 0;
 let computerScore = 0;
 let numGames = 5;
 
-
-function playGame() {
-  
-  function getComputerChoice() {
-    let rock = "rock";
-    let paper = "paper";
-    let scissor = "scissor";
-  
-    let randomly = Math.floor(Math.random() * 100);
-    console.log(randomly);
-  
-    if(randomly <= 33){
-      return rock;
-    } else if (randomly > 33 && randomly <= 66) {
-      return paper;
-    } else {
-      return scissor;
-    }
-  }
+const div = document.querySelector("div");
+const p = document.createElement("p");
 
 
-  function getHumanChoice() {
-    let rock = "rock";
-    let paper = "paper";
-    let scissor = "scissor";
-    
-    let humChoice = prompt("Let's play rock, paper, scissor and see if you win!");
+function getComputerChoice() {
+  let rock = "rock";
+  let paper = "paper";
+  let scissor = "scissor";
 
-    if (humChoice === null) {
-      alert("Too bad you didn't want to participate in our game.");
-      return null;
-  
-    } else if (humChoice.toLowerCase() === "scissor") {
-        console.log(scissor);
-        return scissor;
-    } else if (humChoice.toLowerCase() === "paper") {
-        console.log("paper");
-        return paper;
-    
-    } else if (humChoice.toLowerCase() === "rock"){
-        console.log(rock);
-        return rock;
-    
-    } else {
-        alert("That wasn't any valid choice, please check your spelling.")
-        return getHumanChoice();
-    }
-  }
+  let randomly = Math.floor(Math.random() * 100);
+  console.log(randomly);
 
-  function playRound(humanChoice, computerChoice) {
-    if (humanChoice === "rock" && computerChoice === "rock" || humanChoice === "scissor" && computerChoice === "scissor" || humanChoice === "paper" && computerChoice === "paper") {
-      console.log("This is a draw, no points awarded.");
-    
-    } else if (humanChoice === "rock" && computerChoice === "scissor" || humanChoice === "scissor" && computerChoice === "paper" || humanChoice === "paper" && computerChoice === "rock") {
-        humanScore += 1;
-    
-    } else if (humanChoice === "rock" && computerChoice === "paper" || humanChoice === "paper" && computerChoice === "scissor" || humanChoice === "scissor" && computerChoice === "rock") {
-        computerScore += 1;
-    }
-  }
-
-  for (let index = 0; index < numGames; index++) {
-    const computerSelection = getComputerChoice(); 
-    const humanSelection = getHumanChoice();
-    playRound(humanSelection, computerSelection);
-    console.log(humanSelection);
-    if (humanSelection === null) {
-      console.log(`Score ends: User ${humanScore} points | Computer ${computerScore} points`);
-      break;
-    }
-    console.log(`Score is: User ${humanScore} points | Computer ${computerScore} points`);
-  }
-  
-
-  if (humanScore === computerScore) {
-    console.log("No winner this time - it ends in a draw.");
-  } else if (humanScore > computerScore){
-    console.log("You did it, you overcame the machine and won!!");
+  if(randomly <= 33){
+    return rock;
+  } else if (randomly > 33 && randomly <= 66) {
+    return paper;
   } else {
-    console.log("Sorry but no champagne for you, the computer outsmarted you.");
+    return scissor;
   }
 }
 
-playGame();
+function playRound(humanChoice, computerChoice) {
+  if (humanChoice === "rock" && computerChoice === "rock" || humanChoice === "scissor" && computerChoice === "scissor" || humanChoice === "paper" && computerChoice === "paper") {
+    console.log("This is a draw, no points awarded.");
+  
+  } else if (humanChoice === "rock" && computerChoice === "scissor" || humanChoice === "scissor" && computerChoice === "paper" || humanChoice === "paper" && computerChoice === "rock") {
+      humanScore += 1;
+  
+  } else if (humanChoice === "rock" && computerChoice === "paper" || humanChoice === "paper" && computerChoice === "scissor" || humanChoice === "scissor" && computerChoice === "rock") {
+      computerScore += 1;
+  }
+
+  p.textContent = `Score is: User ${humanScore} points | Computer ${computerScore} points`;
+  div.appendChild(p);
+}
+
+const rock = document.querySelector(".rock");
+const paper = document.querySelector(".paper");
+const scissor = document.querySelector(".scissor");
+
+rock.addEventListener("click", () => {
+  playRound("rock", getComputerChoice());
+});
+
+paper.addEventListener("click", () => {
+  playRound("paper", getComputerChoice());
+});
+
+scissor.addEventListener("click", () => {
+  playRound("scissor", getComputerChoice());
+});
+
+// if (humanSelection === null) {
+//   console.log(`Score ends: User ${humanScore} points | Computer ${computerScore} points`);
+// }
+// console.log(`Score is: User ${humanScore} points | Computer ${computerScore} points`);
+
+
+
+// if (humanScore === computerScore) {
+//   console.log("No winner this time - it ends in a draw.");
+// } else if (humanScore > computerScore){
+//   console.log("You did it, you overcame the machine and won!!");
+// } else {
+//   console.log("Sorry but no champagne for you, the computer outsmarted you.");
+// }
+
+// function playGame() {
+  
+  
+
+
+//   function getHumanChoice(hc) {
+    
+
+
+    
+//     let humChoice = hc;
+
+//     if (humChoice === null) {
+//       alert("Too bad you didn't want to participate in our game.");
+//       return null;
+  
+//     } else if (humChoice.toLowerCase() === "scissor") {
+//         console.log(scissor);
+//         return scissor;
+//     } else if (humChoice.toLowerCase() === "paper") {
+//         console.log("paper");
+//         return paper;
+    
+//     } else if (humChoice.toLowerCase() === "rock"){
+//         console.log(rock);
+//         return rock;
+    
+//     } else {
+//         alert("That wasn't any valid choice, please check your spelling.")
+//         return getHumanChoice();
+//     }
+//   }
+
+  
+//   const humanSelection = getHumanChoice();
+//   playRound(humanSelection, computerSelection);
+//   console.log(humanSelection);
+  
+// }
+
+// playGame();
